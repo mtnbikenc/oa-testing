74e30b61c5c2081cf920b10d7dd08d9f48edb5c8
Russell Teague <rteague@redhat.com>
2018-05-16 16:44:10 -0400

    Standardize cloning scripts

--- scripts/clone-ansible.sh
+++ scripts/clone-ansible.sh
@@ -4,27 +4,35 @@ set -euxo pipefail
 source build_options.sh
 
 echo "### Updating git clone of Ansible ###"
+unset GIT_DIR
+unset GIT_WORK_TREE
 if [ ! -d ansible ]; then
     git clone https://github.com/ansible/ansible.git
 else
-    cd ansible
+    export GIT_DIR=${PWD}/ansible/.git
+    export GIT_WORK_TREE=${PWD}/ansible
+    if [ -d ${GIT_DIR}/rebase-apply ]; then
+        rm -rf ${GIT_DIR}/rebase-apply
+    fi
     git reset --hard
     git clean -fdx
     git checkout devel
     git pull --rebase
-    cd ..
+    git branch | grep -v "devel" | xargs git branch -D || true
+fi
+export GIT_DIR=${PWD}/ansible/.git
+export GIT_WORK_TREE=${PWD}/ansible
+
+# Checkout a pull request
+if [ -v OPT_ANSIBLE_PRNUM ]; then
+    git fetch origin pull/${OPT_ANSIBLE_PRNUM}/merge:PR${OPT_ANSIBLE_PRNUM}
+    git checkout PR${OPT_ANSIBLE_PRNUM}
 fi
 
 # Checkout a tag
 if [ -v OPT_ANSIBLE_TAG ]; then
-    cd ansible
     git checkout ${OPT_ANSIBLE_TAG}
-    git submodule update --init --recursive
-    cd ..
 fi
 
-# Checkout a pull request and rebase it to devel
-#export PRNUM=32362
-#git fetch origin pull/${PRNUM}/head:PR${PRNUM}
-#git checkout PR${PRNUM}
-#git rebase devel
+git describe
+git log --oneline -5
--- scripts/clone-openshift-ansible.sh
+++ scripts/clone-openshift-ansible.sh
@@ -3,7 +3,7 @@ set -euxo pipefail
 
 source build_options.sh
 
-echo "### Updating clone of openshift-ansible ###"
+echo "### Updating git clone of openshift-ansible ###"
 unset GIT_DIR
 unset GIT_WORK_TREE
 if [ ! -d openshift-ansible ]; then
@@ -23,12 +23,10 @@ fi
 export GIT_DIR=${PWD}/openshift-ansible/.git
 export GIT_WORK_TREE=${PWD}/openshift-ansible
 
-# Checkout a pull request and rebase it to origin
+# Checkout a pull request
 if [ -v OPT_OA_PRNUM ]; then
     git fetch origin pull/${OPT_OA_PRNUM}/merge:PR${OPT_OA_PRNUM}
     git checkout PR${OPT_OA_PRNUM}
-    git describe
-    git log --oneline -5
 fi
 
 # Checkout a tag
@@ -36,11 +34,5 @@ if [ -v OPT_OA_TAG ]; then
     git checkout ${OPT_OA_TAG}
 fi
 
-# Checkout a release branch and update it
-#git checkout release-3.7 && git pull --rebase
-
-# Add a users remote, and checkout a branch
-#git remote add mtnbikenc https://github.com/mtnbikenc/openshift-ansible
-#export TEST_BRANCH=tests-as-filters
-#git fetch mtnbikenc ${TEST_BRANCH}
-#git checkout ${TEST_BRANCH}
+git describe
+git log --oneline -5
