6a85fa75260b28f5b74569dbbd71932370a73d82
Russell Teague <rteague@redhat.com>
2018-09-20 14:04:27 -0400

    Add etcd scale-up scripts

---
+++ scripts/etcd-scaleup.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+source ansible/hacking/env-setup
+
+source build_options.sh
+
+ansible --version
+
+ansible-inventory -i inventory/hosts --list --yaml
+
+playbook_base='openshift-ansible/playbooks'
+
+echo "### Running OpenShift-Ansible etcd Scaleup ###"
+if [[ -s "${playbook_base}/openshift-etcd/scaleup.yml" ]]; then
+    playbook="${playbook_base}/openshift-etcd/scaleup.yml"
+else
+    playbook="${playbook_base}/byo/openshift-etcd/scaleup.yml"
+fi
+
+time ansible-playbook -i inventory/hosts ${playbook} -vvv
+
