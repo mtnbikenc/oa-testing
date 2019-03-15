20933f6ee73939b61cbf5350b9993470d6de13b2
Jeremiah Stuever <jstuever@redhat.com>
2019-03-15 17:44:07 -0400

    libvirt-40: create-cluster without scaleup

---
+++ libvirt-40/compile-installer.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+SCRIPT_TYPE=$(basename -s .sh "$0")
+LOG_DATE=$(date "+%FT%H.%M.%S")
+
+export TAGS=libvirt
+unbuffer ../scripts/compile-installer.sh |& tee logs/${LOG_DATE}-${SCRIPT_TYPE}.log
---
+++ libvirt-40/destroy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+export AWS_PROFILE="openshift-dev"
+
+bin/openshift-install destroy cluster --dir=./assets --log-level=debug
+
+rm -f ./assets/.openshift_install*
+rm -f ./assets/metadata.json
+rm -f ./assets/terraform.tfstate
+rm -f ./assets/disable-bootstrap.tfvars
+
+rm -f extra_vars.yml
+
+rm -f logs/*.log
