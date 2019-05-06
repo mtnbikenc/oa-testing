1b290c0bbcd88501f25b8feb7e45dd54013aa798
Russell Teague <rteague@redhat.com>
2019-05-06 13:39:59 -0400

    aws-40: Improve unscaleup.sh

--- aws-40/unscaleup.sh
+++ aws-40/unscaleup.sh
@@ -9,16 +9,22 @@ export KUBECONFIG=${PWD}/assets/auth/kubeconfig
 #oc delete clusterrolebinding ssh-bastion
 
 # Uncordon CoreOS node
-COREOS_WORKERS=$(oc get nodes -o wide | grep worker | grep CoreOS | awk '{print $1}')
-if [ ! -z "${COREOS_WORKERS}" ]
+COREOS_WORKERS=$(oc get nodes -o wide | grep worker | grep CoreOS | awk '{print $1}' || true)
+if [[ ! -z "${COREOS_WORKERS}" ]]
 then
     oc adm uncordon ${COREOS_WORKERS}
 fi
 
 # Remove CentOS nodes
-CENTOS_WORKERS=$(oc get nodes -o wide | grep CentOS | awk '{print $1}')
-if [ ! -z "${CENTOS_WORKERS}" ]
+CENTOS_WORKERS=$(oc get nodes -o wide | grep CentOS | awk '{print $1}' || true)
+if [[ ! -z "${CENTOS_WORKERS}" ]]
 then
     oc adm drain ${CENTOS_WORKERS} --force --delete-local-data --ignore-daemonsets --timeout=0s
-    oc delete machinesets -n openshift-machine-api $(oc get machinesets -n openshift-machine-api | grep centos | awk '{print $1}')
+fi
+
+# Remove CentOS machine sets
+CENTOS_MACHINE_SETS=$(oc get machinesets -n openshift-machine-api | grep centos | awk '{print $1}' || true)
+if [[ ! -z "${CENTOS_MACHINE_SETS}" ]]
+then
+    oc delete machinesets -n openshift-machine-api ${CENTOS_MACHINE_SETS}
 fi
