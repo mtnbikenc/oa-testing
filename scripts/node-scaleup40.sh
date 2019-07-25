7f51ac43845b4dd995541bc22bc04c6a213bb007
Russell Teague <rteague@redhat.com>
2019-07-25 13:18:34 -0400

    Use the extracted OpenShift Client
    This client helps you develop, build, deploy, and run your applications on any
    OpenShift or Kubernetes cluster. It also includes the administrative
    commands for managing a cluster under the 'adm' subcommand.
    
    To familiarize yourself with OpenShift, login to your cluster and try creating a sample application:
    
        oc login mycluster.mycompany.com
        oc new-project my-example
        oc new-app django-psql-example
        oc logs -f bc/django-psql-example
    
    To see what has been created, run:
    
        oc status
    
    and get a command shell inside one of the created containers with:
    
        oc rsh dc/postgresql
    
    To see the list of available toolchains for building applications, run:
    
        oc new-app -L
    
    Since OpenShift runs on top of Kubernetes, your favorite kubectl commands are also present in oc,
    allowing you to quickly switch between development and debugging. You can also run kubectl directly
    against any OpenShift cluster using the kubeconfig file created by 'oc login'.
    
    For more on OpenShift, see the documentation at https://docs.openshift.com.
    
    To see the full list of commands supported, run 'oc --help'. when running playbooks

--- scripts/node-scaleup40.sh
+++ scripts/node-scaleup40.sh
@@ -5,6 +5,10 @@ source ansible/hacking/env-setup
 
 source build_options.sh
 
+# Use the extracted `oc` when running playbooks
+PATH=$PWD/bin:$PATH
+which oc
+
 ${PYTHON} $(which ansible) --version
 
 ${PYTHON} $(which ansible-inventory) -i inventory/hosts --list --yaml
