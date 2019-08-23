fc2534fecdc29c53df1ee8f2ada597a9c1cfb7f6
Russell Teague <rteague@redhat.com>
2019-08-23 13:25:27 -0400

    Add removal of RHEL machines/machinesets in unscaleup.sh

--- aws-40/unscaleup.sh
+++ aws-40/unscaleup.sh
@@ -29,4 +29,18 @@ then
     oc delete machinesets -n openshift-machine-api ${CENTOS_MACHINE_SETS}
 fi
 
+# Remove RHEL nodes
+RHEL_WORKERS=$(oc get nodes -o wide | grep "Red Hat Enterprise Linux Server" | awk '{print $1}' || true)
+if [[ ! -z "${RHEL_WORKERS}" ]]
+then
+    oc adm drain ${RHEL_WORKERS} --force --delete-local-data --ignore-daemonsets --timeout=0s
+fi
+
+# Remove RHEL machine sets
+RHEL_MACHINE_SETS=$(oc get machinesets -n openshift-machine-api | grep rhel | awk '{print $1}' || true)
+if [[ ! -z "${RHEL_MACHINE_SETS}" ]]
+then
+    oc delete machinesets -n openshift-machine-api ${RHEL_MACHINE_SETS}
+fi
+
 rm -fv inventory/hosts
