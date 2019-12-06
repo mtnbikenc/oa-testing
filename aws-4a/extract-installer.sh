168d723244c14c624183e3fad7b11dcd06fad5d5
Russell Teague <rteague@redhat.com>
2019-12-06 16:30:04 -0500

    aws-4x: Update extract-installer.sh

--- aws-4a/extract-installer.sh
+++ aws-4a/extract-installer.sh
@@ -6,6 +6,6 @@ source build_options.sh
 # Extract the installer binary from the release payload
 pushd bin
 rm -fv o* sha*
-oc adm release extract --tools ${OPT_REGISTRY}:${OPT_PAYLOAD} -a ${OPT_PULL_SECRET}
-for a in $(ls -1 *.tar.gz); do tar -zxvf $a; done
+oc adm release extract --tools "${OPT_REGISTRY}":"${OPT_PAYLOAD}" -a "${OPT_PULL_SECRET}"
+for a in *.tar.gz; do tar -zxvf "$a"; done
 popd
