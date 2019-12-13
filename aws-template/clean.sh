dd95cb372566100a2a7464dce4be5c4b9cbe2b09
Russell Teague <rteague@redhat.com>
2019-12-13 16:04:16 -0500

    Remove aws-template and reset scripts

--- aws-template/clean.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# Clean color codes from log files
-
-for log_file in logs/*.log
-do
-  echo "Stripping colors from ${log_file}"
-  sed -i -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[mGK]//g" ${log_file}
-done
