129b21be96326d23a4c10c7a1a71df824b5f31f2
Russell Teague <rteague@redhat.com>
2019-12-13 16:20:12 -0500

    Improve ssh scripts

--- aws-4b/ssh.sh
+++ aws-4b/ssh.sh
@@ -10,9 +10,10 @@
 source build_options.sh
 
 REMOTEHOST=$(ansible-inventory -i "${OPT_CLUSTER_DIR}/inventory/hosts" --list | jq --argjson index "$1" -r '.new_workers.hosts[$index]')
-export REMOTEHOST
+REMOTEUSER=$(ansible-inventory -i "${OPT_CLUSTER_DIR}/inventory/hosts" --list | jq -r --arg remotehost "$REMOTEHOST" '._meta.hostvars | .[$remotehost] | .ansible_user')
 
-REMOTEUSER=$(ansible-inventory -i "${OPT_CLUSTER_DIR}inventory/hosts" --list | jq -r --arg remotehost "$REMOTEHOST" '._meta.hostvars | .[$remotehost] | .ansible_user')
 export KUBECONFIG=${OPT_CLUSTER_DIR}/assets/auth/kubeconfig
+BASTION=$(oc get service -n byoh-ssh-bastion ssh-bastion -o jsonpath="{.status.loadBalancer.ingress[0].hostname}")
+export BASTION
 
-ssh -o IdentityFile="${OPT_PRIVATE_KEY}" -o StrictHostKeyChecking=no -o ProxyCommand='ssh -A -o StrictHostKeyChecking=no -o ServerAliveInterval=30 -W %h:%p core@$(oc get service -n byoh-ssh-bastion ssh-bastion -o jsonpath="{.status.loadBalancer.ingress[0].hostname}")' "${REMOTEUSER}"@"${REMOTEHOST}"
+ssh -o IdentityFile="${OPT_PRIVATE_KEY}" -o StrictHostKeyChecking=no -o ProxyCommand='ssh -A -o StrictHostKeyChecking=no -o ServerAliveInterval=30 -W %h:%p core@${BASTION}' "${REMOTEUSER}"@"${REMOTEHOST}"
