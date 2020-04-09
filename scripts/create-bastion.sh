1aae383d8d904e73217a8482126de87887a6c220
Russell Teague <rteague@redhat.com>
2020-04-09 11:26:57 -0400

    Update ssh-bastion source

--- scripts/create-bastion.sh
+++ scripts/create-bastion.sh
@@ -7,4 +7,4 @@ export KUBECONFIG=${OPT_CLUSTER_DIR}/assets/auth/kubeconfig
 
 echo "Setting up ssh bastion host..."
 export SSH_BASTION_NAMESPACE=test-ssh-bastion
-curl https://raw.githubusercontent.com/mtnbikenc/ssh-bastion/fix-rolebinding/deploy/deploy.sh | bash
+curl https://raw.githubusercontent.com/eparis/ssh-bastion/master/deploy/deploy.sh | bash -x
