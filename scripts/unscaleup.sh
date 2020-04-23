2b326b7a660e78527c5af043a28c020a6bbed50e
Russell Teague <rteague@redhat.com>
2020-04-23 11:33:06 -0400

    aws-4: Use extracted oc bin in unscaleup.sh

--- scripts/unscaleup.sh
+++ scripts/unscaleup.sh
@@ -4,45 +4,53 @@ set -euxo pipefail
 source build_options.sh
 
 export KUBECONFIG=${OPT_CLUSTER_DIR}/assets/auth/kubeconfig
+export OC_BIN=./bin/oc
 
 # Remove ssh bastion
-#oc delete project test-ssh-bastion
-#oc delete clusterrole ssh-bastion
-#oc delete clusterrolebinding ssh-bastion
+#${OC_BIN} delete project test-ssh-bastion
+#${OC_BIN} delete clusterrole ssh-bastion
+#${OC_BIN} delete clusterrolebinding ssh-bastion
 
-# Uncordon CoreOS node
-mapfile -t COREOS_WORKERS < <(oc get nodes --output wide | grep worker | grep CoreOS | awk '{print $1}' || true)
+# Uncordon CoreOS nodes
+mapfile -t COREOS_WORKERS < <(${OC_BIN} get nodes --output wide | grep worker | grep CoreOS | awk '{print $1}' || true)
 if [[ ${#COREOS_WORKERS[@]} != 0 ]]
 then
-    oc adm uncordon "${COREOS_WORKERS[@]}"
+    ${OC_BIN} adm uncordon "${COREOS_WORKERS[@]}"
 fi
 
-# Remove CentOS nodes
-mapfile -t CENTOS_WORKERS < <(oc get nodes --output wide | grep CentOS | awk '{print $1}' || true)
+# Drain CentOS nodes
+mapfile -t CENTOS_WORKERS < <(${OC_BIN} get nodes --output wide | grep CentOS | awk '{print $1}' || true)
 if [[ ${#CENTOS_WORKERS[@]} != 0 ]]
 then
-    oc adm drain "${CENTOS_WORKERS[@]}" --force --delete-local-data --ignore-daemonsets --timeout=0s
+    ${OC_BIN} adm drain "${CENTOS_WORKERS[@]}" --force --delete-local-data --ignore-daemonsets --timeout=0s
 fi
 
 # Remove CentOS machine sets
-mapfile -t CENTOS_MACHINE_SETS < <(oc get machinesets -n openshift-machine-api | grep centos | awk '{print $1}' || true)
+mapfile -t CENTOS_MACHINE_SETS < <(${OC_BIN} get machinesets -n openshift-machine-api | grep centos | awk '{print $1}' || true)
 if [[ ${#CENTOS_MACHINE_SETS[@]} != 0 ]]
 then
-    oc delete machinesets --namespace openshift-machine-api "${CENTOS_MACHINE_SETS[@]}"
+    ${OC_BIN} delete machinesets --namespace openshift-machine-api "${CENTOS_MACHINE_SETS[@]}"
 fi
 
-# Remove RHEL nodes
-mapfile -t RHEL_WORKERS < <(oc get nodes -o wide | grep "Red Hat Enterprise Linux Server" | awk '{print $1}' || true)
+# Drain RHEL nodes
+mapfile -t RHEL_WORKERS < <(${OC_BIN} get nodes -o wide | grep "Red Hat Enterprise Linux Server" | awk '{print $1}' || true)
 if [[ ${#RHEL_WORKERS[@]} != 0 ]]
 then
-    oc adm drain "${RHEL_WORKERS[@]}" --force --delete-local-data --ignore-daemonsets --timeout=0s
+    ${OC_BIN} adm drain "${RHEL_WORKERS[@]}" --force --delete-local-data --ignore-daemonsets --timeout=0s
 fi
 
 # Remove RHEL machine sets
-mapfile -t RHEL_MACHINE_SETS < <(oc get machinesets --namespace openshift-machine-api | grep rhel | awk '{print $1}' || true)
+mapfile -t RHEL_MACHINE_SETS < <(${OC_BIN} get machinesets --namespace openshift-machine-api | grep rhel | awk '{print $1}' || true)
 if [[ ${#RHEL_MACHINE_SETS[@]} != 0 ]]
 then
-    oc delete machinesets --namespace openshift-machine-api "${RHEL_MACHINE_SETS[@]}"
+    ${OC_BIN} delete machinesets --namespace openshift-machine-api "${RHEL_MACHINE_SETS[@]}"
+fi
+
+# Delete RHEL nodes (nodes not part of machinesets will not be automatically deleted)
+mapfile -t RHEL_WORKERS < <(${OC_BIN} get nodes --output wide | grep worker | grep "Red Hat Enterprise Linux Server" | awk '{print $1}' || true)
+if [[ ${#RHEL_WORKERS[@]} != 0 ]]
+then
+    ${OC_BIN} delete node "${RHEL_WORKERS[@]}"
 fi
 
 time ansible-playbook -i localhost, ../playbooks/terminate.yml -vvv -e ansible_python_interpreter=${LOCAL_PYTHON}
