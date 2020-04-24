d2277e4c798ceffb8da4be5633b41c87eeeb7c48
Russell Teague <rteague@redhat.com>
2020-04-24 09:20:19 -0400

    aws-4: Use Ansible out of checkout for some local 4.x playbooks

--- scripts/create-machines.sh
+++ scripts/create-machines.sh
@@ -3,4 +3,11 @@ set -euxo pipefail
 
 source build_options.sh
 
-ansible-playbook -i "${OPT_CLUSTER_DIR}/inventory/hosts" ../playbooks/create-machines.yml -vvv
+# shellcheck source=/dev/null
+source "${OPT_CLUSTER_DIR}/ansible/hacking/env-setup"
+
+# Use the extracted `oc` when running playbooks
+PATH=${OPT_CLUSTER_DIR}/bin:$PATH
+command -v oc
+
+time ${LOCAL_PYTHON} "$(command -v ansible-playbook)" -i "${OPT_CLUSTER_DIR}/inventory/hosts" ../playbooks/create-machines.yml -vvv
--- scripts/provision40.sh
+++ scripts/provision40.sh
@@ -3,4 +3,7 @@ set -euxo pipefail
 
 source build_options.sh
 
-time ansible-playbook -i localhost, ../playbooks/provision40.yml -vvv -e ansible_python_interpreter=${LOCAL_PYTHON}
+# shellcheck source=/dev/null
+source "${OPT_CLUSTER_DIR}/ansible/hacking/env-setup"
+
+time ${LOCAL_PYTHON} "$(command -v ansible-playbook)" -i localhost, ../playbooks/provision40.yml -vvv -e ansible_python_interpreter=${LOCAL_PYTHON}
