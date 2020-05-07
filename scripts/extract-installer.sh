814d7b3ae742ba5e9dde0a2d56ca748145fef7fa
Russell Teague <rteague@redhat.com>
2020-05-07 13:11:01 -0400

    aws-4: Improve extract-installer.sh

--- scripts/extract-installer.sh
+++ scripts/extract-installer.sh
@@ -6,7 +6,10 @@ source build_options.sh
 # Extract the installer binary from the release payload
 mkdir --parents "${OPT_CLUSTER_DIR}/bin"
 pushd "${OPT_CLUSTER_DIR}/bin"
-rm --force --verbose o* sha*
-oc adm release extract --tools "${OPT_REGISTRY}":"${OPT_PAYLOAD}" -a "${OPT_PULL_SECRET}"
+rm --force --verbose ./*
+oc adm release extract --tools "${OPT_RELEASE_IMAGE}" --registry-config "${OPT_PULL_SECRET}"
 for a in *.tar.gz; do tar -zxvf "$a"; done
+# https://www.terraform.io/downloads.html
+#wget -q https://releases.hashicorp.com/terraform/0.12.23/terraform_0.12.23_linux_amd64.zip
+#unzip ./*.zip
 popd
