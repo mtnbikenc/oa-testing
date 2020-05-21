85d59f6f62140ff79f2197107bc208eb22a025f4
Russell Teague <rteague@redhat.com>
2020-05-21 09:58:41 -0400

    aws-4: Use Origin e2e-tests from image

--- scripts/e2e-tests.sh
+++ scripts/e2e-tests.sh
@@ -5,26 +5,5 @@ source build_options.sh
 
 export KUBECONFIG=${OPT_CLUSTER_DIR}/assets/auth/kubeconfig
 
-pushd "${GOPATH}/src/github.com/openshift/openshift-tests"
-
-EXISTING_HASH=$(git rev-parse HEAD)
-git reset --hard
-git checkout master
-git pull --rebase
-git fetch --all --tags --prune
-git branch | grep -v "master" | xargs git branch -D || true
-git checkout "release-${OPT_PAYLOAD}"
-CURRENT_HASH=$(git rev-parse HEAD)
-if [ "${EXISTING_HASH}" != "${CURRENT_HASH}" ] || [ ! -e "./extended-platform-tests" ]
-then
-  go build ./cmd/extended-platform-tests
-fi
-
-popd
-
-mkdir --parents "${OPT_CLUSTER_DIR}/bin"
-cp "${GOPATH}/src/github.com/openshift/openshift-tests/extended-platform-tests" ./bin
-export PATH="${PWD}/bin:$PATH"
-
-./bin/extended-platform-tests run openshift/conformance/parallel
-#./bin/extended-platform-tests run -f e2e-failing.txt
+podman pull "registry.svc.ci.openshift.org/origin/${OPT_PAYLOAD}:tests"
+podman run -v "${KUBECONFIG}:/root/.kube/config:z" -i "registry.svc.ci.openshift.org/origin/${OPT_PAYLOAD}:tests" /bin/bash -c 'export KUBECONFIG=/root/.kube/config && openshift-tests run openshift/conformance/parallel'
