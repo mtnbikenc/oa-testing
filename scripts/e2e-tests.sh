eaf1139986d469636be9003f2e192cecd575b870
Russell Teague <rteague@redhat.com>
2020-08-21 10:23:37 -0400

    Clean up e2e-tests script

--- scripts/e2e-tests.sh
+++ scripts/e2e-tests.sh
@@ -6,4 +6,7 @@ source build_options.sh
 export KUBECONFIG=${OPT_CLUSTER_DIR}/assets/auth/kubeconfig
 
 podman pull "registry.svc.ci.openshift.org/origin/${OPT_PAYLOAD}:tests"
-podman run -v "${KUBECONFIG}:/root/.kube/config:z" -i "registry.svc.ci.openshift.org/origin/${OPT_PAYLOAD}:tests" /bin/bash -c 'export KUBECONFIG=/root/.kube/config && openshift-tests run openshift/conformance/parallel'
+podman run --rm --interactive \
+  --volume "${KUBECONFIG}:/root/.kube/config:z" \
+  "registry.svc.ci.openshift.org/origin/${OPT_PAYLOAD}:tests" \
+  /bin/bash -c 'export KUBECONFIG=/root/.kube/config && openshift-tests run openshift/conformance/parallel'
