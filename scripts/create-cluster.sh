5f5c0b76572a130249ce6dcc5af4e77e4caff9a9
Russell Teague <rteague@redhat.com>
2020-08-21 10:27:18 -0400

    Add note about terraform debugging to create-cluster script

--- scripts/create-cluster.sh
+++ scripts/create-cluster.sh
@@ -3,4 +3,5 @@ set -euxo pipefail
 
 source build_options.sh
 
+#export TF_LOG=debug  # Enable this for terraform debugging
 "${OPT_CLUSTER_DIR}/bin/openshift-install" create cluster --dir="${OPT_CLUSTER_DIR}/assets" --log-level=debug
