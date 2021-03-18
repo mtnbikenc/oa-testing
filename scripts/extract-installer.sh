6fe8944a5b5f97043d7e7b0f9137694471f8ffdb
Russell Teague <rteague@redhat.com>
2021-03-18 13:28:52 -0400

    aws-4: Verify release signatures

--- scripts/extract-installer.sh
+++ scripts/extract-installer.sh
@@ -8,6 +8,7 @@ mkdir --parents "${OPT_CLUSTER_DIR}/bin"
 pushd "${OPT_CLUSTER_DIR}/bin"
 rm --force --verbose ./*
 oc adm release extract --tools "${OPT_RELEASE_IMAGE}" --registry-config "${OPT_PULL_SECRET}"
+sha256sum --check sha256sum.txt
 for a in *.tar.gz; do tar -zxvf "$a"; done
 # https://www.terraform.io/downloads.html
 #wget -q https://releases.hashicorp.com/terraform/0.12.23/terraform_0.12.23_linux_amd64.zip
